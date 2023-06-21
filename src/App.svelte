20a22ea19156cb7610271ab64e6c76b0bce4e9d7
FoucheHugo <hugo.fouche@reseau.eseo.fr>
2023-06-21 04:16:37 +0200

    initial commut

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import "./style/output/main.css";
+  import Copyrights from "./components/copyrights/Copyrights.svelte";
+</script>
+
+<svelte:head>
+    <title>Draft League of Legends</title>
+</svelte:head>
+
+<Copyrights />
