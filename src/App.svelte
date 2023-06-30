b10f186a41cfc22e01ecad6dbae1cd3d455eafac
FoucheHugo <hugo.fouche@reseau.eseo.fr>
2023-06-30 21:18:38 +0200

    lighthouse fix

--- src/App.svelte
+++ src/App.svelte
@@ -2,14 +2,8 @@
     import './style/output/main.css'
     import Copyrights from './components/copyrights/Copyrights.svelte'
     import Draft from './components/draft/Draft.svelte'
-    import logo from '/logo.png'
     import Footer from './components/footer/Footer.svelte'
 </script>
-
-<svelte:head>
-    <link rel="icon" href={logo} />
-    <title>Draft League of Legends</title>
-</svelte:head>
 
 <Draft />
 <Footer />
